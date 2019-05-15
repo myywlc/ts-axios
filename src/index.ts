4737aade9a77eb244d717d45c3de4689b52f2d48
凌 <myywlc@163.com>
2019-05-16 00:26:31 +0800

    实现post请求

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
 import { bulidURL } from './helpers/url'
+import { transformRequest } from './helpers/data'
+import { processHeaders } from './helpers/headers'
 
 function axios (config: AxiosRequestConfig): void {
   processConfig(config)
@@ -9,11 +11,22 @@ function axios (config: AxiosRequestConfig): void {
 
 function processConfig (config: AxiosRequestConfig): void {
   config.url = transformUrl(config)
+  config.headers = transformHeaders(config)
+  config.data = transformRequestData(config)
 }
 
 function transformUrl (config: AxiosRequestConfig): string {
   const { url, params } = config
   return bulidURL(url, params)
+}
+
+function transformRequestData (config: AxiosRequestConfig): any {
+  return transformRequest(config.data)
+}
+
+function transformHeaders (config: AxiosRequestConfig) {
+  const { headers = {}, data } = config
+  return processHeaders(headers, data)
 }
 
 export default axios
