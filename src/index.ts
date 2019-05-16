2a05bd01c6bea7cb7aefd5f1312ba4d72f376883
凌 <myywlc@163.com>
2019-05-16 23:55:34 +0800

    获取数据

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import { AxiosRequestConfig } from './types'
+import { AxiosPromise, AxiosRequestConfig, AxiosResponse } from './types'
 import xhr from './xhr'
 import { bulidURL } from './helpers/url'
-import { transformRequest } from './helpers/data'
+import { transformRequest, transformResponse } from './helpers/data'
 import { processHeaders } from './helpers/headers'
 
-function axios (config: AxiosRequestConfig): void {
+function axios (config: AxiosRequestConfig): AxiosPromise {
   processConfig(config)
-  xhr(config)
+  return xhr(config).then((res) => transformResponseData(res))
 }
 
 function processConfig (config: AxiosRequestConfig): void {
@@ -27,6 +27,11 @@ function transformRequestData (config: AxiosRequestConfig): any {
 function transformHeaders (config: AxiosRequestConfig) {
   const { headers = {}, data } = config
   return processHeaders(headers, data)
+}
+
+function transformResponseData(res: AxiosResponse): AxiosResponse {
+  res.data = transformResponse(res.data)
+  return res
 }
 
 export default axios
